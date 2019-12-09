814d99366e0ba12f10ba40e4ef5af32ee7662086
hatout01 <hatout1@gmail.com>
2019-12-08 17:24:19 -0800

    fixed function for the game and another function to restart the game

--- index.js
+++ index.js
@@ -22,3 +22,110 @@ var correctLetters = [];
 // Guesses left
 var guessesLeft = 10;
 
+function knowledge() {
+
+    // Generates new word for Word constructor if true
+    if (requireNewWord) {
+        // Selects random from wordsArr array
+        var randomWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+
+        // Passes random word through the Word constructor
+        computerWord = new Word(randomWord);
+        requireNewWord = false;
+    }
+
+    // Test if the letter guessed is correct
+    var wordComplete = [];
+    computerWord.objArray.forEach(completeCheck);
+
+    // letters remaining to be guessed
+    if (wordComplete.includes(false)) {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "let's find it! Guess a letter between A-Z!",
+                    name: "userinput"
+                }
+            ])
+            .then(function (input) {
+                if (!letterArray.includes(input.userinput) || input.userinput.length > 1) {
+                    console.log("\nPlease try again!\n");
+                    knowledge();
+                } else {
+                    if (incorrectLetters.includes(input.userinput) || correctLetters.includes(input.userinput) || input.userinput === "") {
+                        console.log("\nAlready Guessed or Nothing Entered\n");
+                        knowledge();
+                    } else {
+                        // Checks if guess is correct
+                        var wordCheckArray = [];
+
+                        computerWord.userGuess(input.userinput);
+
+                        // Checks if guess is correct
+                        computerWord.objArray.forEach(wordCheck);
+                        if (wordCheckArray.join('') === wordComplete.join('')) {
+                            console.log("\nIncorrect\n");
+                            incorrectLetters.push(input.userinput);
+                            guessesLeft--;
+                        } else {
+                            console.log("\nCorrect!\n");
+                            correctLetters.push(input.userinput);
+                        }
+
+                        computerWord.log();
+                        // Print guesses left
+                        console.log("Guesses Left: " + guessesLeft + "\n");
+
+                        // Print letters guessed already
+                        console.log("Letters Guessed: " + incorrectLetters.join(" ") + "\n");
+
+                        // Guesses left
+                        if (guessesLeft > 0) {
+                            knowledge();
+                        } else {
+                            console.log("Sorry, you lose!\n");
+                            restartGame();
+                        }
+
+                        function wordCheck(key) {
+                            wordCheckArray.push(key.guessed);
+                        }
+                    }
+                }
+            })
+    } else {
+        console.log("YOU WIN!\n");
+        restartGame();
+    }
+
+    function completeCheck(key) {
+        wordComplete.push(key.guessed);
+    }
+
+}
+
+function restartGame() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to:",
+                choices: ["Play Again", "Exit"],
+                name: "restart"
+            }
+        ])
+        .then(function (input) {
+            if (input.restart === "Play Again") {
+                requireNewWord = true;
+                incorrectLetters = [];
+                correctLetters = [];
+                guessesLeft = 10;
+                knowledge();
+            } else {
+                return
+            }
+        })
+}
+
+knowledge();
